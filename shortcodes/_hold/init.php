7474f8cf5204f48619bc2cc8dd6d974f50e2a4c6
shawnchollette <shawnchollette@gmail.com>
2016-05-09 14:34:51 -0500

    add files
    just adding files

---
+++ shortcodes/_hold/init.php
@@ -0,0 +1,10 @@
+<?php 	
+function shortcode_stuff() {
+//	wp_enqueue_style( 'tbwa_shortcode_css', plugins_url('shortcode.css', __FILE__));
+//	wp_enqueue_style( 'tbwa_shortcode_css', plugins_url( 'shortcode.css' , dirname(__FILE__) ));
+	 
+	wp_enqueue_style('biotabs_css',plugins_url('/',__FILE__).'shortcode.css');	
+	
+//	echo '<img src="' . plugins_url( 'images/wordpress.png' , dirname(__FILE__) ) . '" > ';
+}
+add_action('wp_enqueue_scripts', 'shortcode_stuff');
---
+++ shortcodes/init.php
@@ -0,0 +1,5 @@
+<?php 	
+function shortcode_css() {
+	wp_enqueue_style('shortcode_css',plugins_url('/',__FILE__).'shortcode.css');		
+}
+add_action('wp_enqueue_scripts', 'shortcode_css');
